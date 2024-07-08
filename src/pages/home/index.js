9637469af28eaed92aeebc016d8df001eac1d93e
Walker Li <lazyrf.tw@gmail.com>
2024-07-09 00:29:49 +0800

    Using antd table to show data

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,34 @@
-import React, {useEffect} from 'react';
-import {Row, Col, Card} from 'antd';
+import React, {useEffect, useState} from 'react';
+import {Row, Col, Card, Table} from 'antd';
 import './home.css';
 import {getData} from '../../api';
 
+const columns = [
+    {
+        title: '課程',
+        dataIndex: 'name',
+    },
+    {
+        title: '今日購買',
+        dataIndex: 'todayBuy',
+    },
+    {
+        title: '本月購買',
+        dataIndex: 'monthBuy',
+    },
+    {
+        title: '總購買',
+        dataIndex: 'totalBuy',
+    },
+];
+
 const Home = () => {
+    const [tableData, setTableData] = useState([]);
+
     useEffect(() => {
-        getData().then((res) => {
-            console.log(res);
+        getData().then(({data}) => {
+            const {tableData} = data.data;
+            setTableData(tableData);
         });
     }, []);
 
@@ -25,6 +47,9 @@ const Home = () => {
                         <p>上次登入時間:<span>2024-07-08</span></p>
                         <p>上次登入地點:<span>台中南屯區</span></p>
                     </div>
+                </Card>
+                <Card>
+                    <Table dataSource={tableData} columns={columns} pagination={false} rowKey={'name'} />
                 </Card>
             </Col>
             <Col span={16}></Col>
