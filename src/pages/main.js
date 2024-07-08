9723010fde18d03caabf67ddddcc6f085ffdc55f
Walker <lazyrf.tw@gmail.com>
2024-07-08 16:45:16 +0800

    IMPORTANT!!! Add the Outlet

--- src/pages/main.js
+++ src/pages/main.js
@@ -37,7 +37,7 @@ const Main = () => {
                             borderRadius: borderRadiusLG,
                     }}
                 >
-                    Content
+                    <Outlet />
                 </Content>
             </Layout>
         </Layout>
