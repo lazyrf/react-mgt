9efc3381736fcdb3bdf4239993565eeaed57f19f
Walker <lazyrf.tw@gmail.com>
2024-07-14 10:52:51 +0800

    Add getUser mock api

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,5 +1,7 @@
 import Mock from 'mockjs';
 import homeApi from './mockServerData/home';
+import userApi from './mockServerData/user';
 
 // 攔截接口
 Mock.mock(/home\/getData/, homeApi.getStatisticalData)
+Mock.mock(/user\/getUser/, userApi.getUserList)
